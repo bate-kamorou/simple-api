ddcaff5e51064002049e5e3db184a59210c4dd32
kamorou bate <kamoroubate1@gmail.com>
2026-03-07 15:22:28 +0000

    added a post endpoint with pydantic type check

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -12,11 +13,18 @@ def sayhello():
 def greetings(name:str):
     return {"message": f"hello {name} nice to meet you 🤝"}
 
-# use pydantic basemodel for type validation
-
-
 # add endpoint (multi query parameters)
 @app.get("/add")
 def add_nums(a:float, b:float):
     result = a + b
     return {"Response" : f"Sum of a:{a} and b:{b} is equals to: {result}"}
+
+# add a post enpoint 
+class Nums(BaseModel):
+    a:float
+    b:float
+
+@app.post("/add-json")
+def multiply_json(numbers: Nums):
+    result = numbers.a * numbers.b
+    return {"result": result}
